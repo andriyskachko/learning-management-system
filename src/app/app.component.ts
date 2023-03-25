c0399d188a78f5cad63bf3dc84f6a0e6bd8abf5f
Andriy Skachko <96697847+andriyskachko@users.noreply.github.com>
2023-03-25 20:43:30 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'learning-management-system-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'learning-management-system';
+}
